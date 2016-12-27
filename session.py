c180781849206f0893191abb23d2f1cb0f047a3c
Lori Baumgartner <lori.burleson@gmail.com>
2016-12-27 12:30:14 -0600

    update naming from colors to answers, add logic for receiving responses from human, add needed templates

--- session.py
+++ session.py
@@ -1,4 +1,5 @@
 import logging
+from random import choice
 
 from flask import Flask, json, render_template
 from flask_ask import Ask, request, session, question, statement
@@ -8,9 +9,10 @@ app = Flask(__name__)
 ask = Ask(app, "/")
 logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
+with open('speech_assets/customSlotTypes/LIST_OF_ANSWERS') as f:
+     answers = [a.strip() for a in f.readlines()]
 
-COLOR_KEY = "COLOR"
-
+# AMAZON.NoIntent | AMAZON.RepeatIntent | AMAZON.YesIntent
 
 @ask.launch
 def launch():
@@ -20,29 +22,62 @@ def launch():
     return question(question_text).reprompt(reprompt_text).simple_card(card_title, question_text)
 
 
-@ask.intent('MyColorIsIntent', mapping={'color': 'Color'})
-def my_color_is(color):
-    card_title = render_template('card_title')
-    if color is not None:
-        session.attributes[COLOR_KEY] = color
-        question_text = render_template('known_color', color=color)
-        reprompt_text = render_template('known_color_reprompt')
-    else:
-        question_text = render_template('unknown_color')
-        reprompt_text = render_template('unknown_color_reprompt')
-    return question(question_text).reprompt(reprompt_text).simple_card(card_title, question_text)
+@ask.intent('AMAZON.NoIntent')
+def no():
+    no_text = render_template('no')
+    return statement(no_text)
 
+@ask.intent('AMAZON.YesIntent')
+def yes():
+    actual_answer = random.choice(answers)
+    yes_text = render_template('yes', actual_answer=actual_answer)
+    return question(yes_text)
 
-@ask.intent('WhatsMyColorIntent')
-def whats_my_color():
+@ask.intent('AnswerIntent', mapping={'answer': 'Answer'})
+def my_answer_is(answer):
     card_title = render_template('card_title')
-    color = session.attributes.get(COLOR_KEY)
-    if color is not None:
-        statement_text = render_template('known_color_bye', color=color)
-        return statement(statement_text).simple_card(card_title, statement_text)
+
+    if answer is not None:
+        if answer == actual_answer:
+            response_text = render_template('correct_response', actual_answer=answer)
+            reprompt_text = render_template('welcome_reprompt')
+            return statement(response_text).question(reprompt_text)
+        else:
+            response_text = render_template('incorrect_response', actual_answer=answer)
+            reprompt_text = render_template('welcome_reprompt')
+            return statement(response_text).question(reprompt_text)
     else:
-        question_text = render_template('unknown_color_reprompt')
-        return question(question_text).reprompt(question_text).simple_card(card_title, question_text)
+        yes_text = render_template('yes', actual_answer=actual_answer)
+        return question(yes_text)
+
+@ask.intent('AnswerOnlyIntent', mapping={'answer': 'Answer'})
+def only_answer_is(answer):
+    card_title = render_template('card_title')
+
+    if answer is not None:
+        if answer == actual_answer:
+            response_text = render_template('correct_response', actual_answer=answer)
+            reprompt_text = render_template('welcome_reprompt')
+            return statement(response_text).question(reprompt_text)
+        else:
+            response_text = render_template('incorrect_response', actual_answer=answer)
+            reprompt_text = render_template('welcome_reprompt')
+            return statement(response_text).question(reprompt_text)
+    else:
+        yes_text = render_template('yes', actual_answer=actual_answer)
+        return question(yes_text)
+
+# @ask.intent('WhatsMyColorIntent')
+# def whats_my_color():
+#     card_title = render_template('card_title')
+#     color = session.attributes.get(COLOR_KEY)
+#     if color is not None:
+#         statement_text = render_template('known_color_bye', color=color)
+#         return statement(statement_text).simple_card(card_title, statement_text)
+#     else:
+#         question_text = render_template('unknown_color_reprompt')
+#         return question(question_text).reprompt(question_text).simple_card(card_title, question_text)
+
 
 
 @ask.session_ended
